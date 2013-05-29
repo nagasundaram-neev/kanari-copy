ab3ca042c8171b05b33207e807ba51d347d299b6
Emil Soman <emil.soman@gmail.com>
2013-05-29 16:44:53 +0530

    Add 'email' to Customers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130529103838) do
+ActiveRecord::Schema.define(version: 20130529111339) do
 
   create_table "cuisine_types", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20130529103838) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "customer_admin_id"
+    t.string   "email"
   end
 
   create_table "feedbacks", force: true do |t|
