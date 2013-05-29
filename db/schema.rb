7f3e6e55b072907112e93b989ebdd473fa355438
Emil Soman <emil.soman@gmail.com>
2013-05-29 12:31:42 +0530

    Add phone_number field to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130515132343) do
+ActiveRecord::Schema.define(version: 20130529070043) do
 
   create_table "cuisine_types", force: true do |t|
     t.string   "name"
@@ -177,6 +177,7 @@ ActiveRecord::Schema.define(version: 20130515132343) do
     t.string   "role"
     t.integer  "points_available"
     t.integer  "points_redeemed"
+    t.string   "phone_number"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
