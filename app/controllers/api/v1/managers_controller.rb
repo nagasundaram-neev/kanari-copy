d5ca6cb5142b99b714211150a75f8a644d59e133
Emil Soman <emil.soman@gmail.com>
2013-06-12 19:19:00 +0530

    List managers API should list unassigned managers as well

--- app/controllers/api/v1/managers_controller.rb
+++ app/controllers/api/v1/managers_controller.rb
@@ -20,7 +20,9 @@ class Api::V1::ManagersController < ApplicationController
   def index
     authorize! :read, User
     #TODO: Query to be optimized
-    managers = current_user.customer.outlets.collect(&:manager).uniq rescue []
+    assigned_managers = current_user.customer.outlets.collect(&:manager).uniq rescue []
+    unassigned_managers = User.includes(:employed_customer).where(customers: {id: current_user.customer.id}) rescue []
+    managers = assigned_managers + unassigned_managers
     render json: managers, each_serializer: ManagerSerializer
   end
 
