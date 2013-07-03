18d906f7e6215d04529197b16d4bbf714e4e439b
Priya <priya@neevtech.com>
2013-07-03 15:41:55 +0530

    tablet app page settings

---
+++ app/assets/javascripts/controllers_tablet.js
@@ -0,0 +1,12 @@
+module.controller('commonCtrl', function($scope, $http, $location) {
+	//$location.url('/home');
+	$scope.emailCLick = function() {
+		$location.url("/login");
+	};
+	$scope.signUpCLick = function() {
+		$location.url("/signUp");
+	};
+	$scope.leaveFeedback = function() {
+		$location.url("/feedback");
+	};
+});
