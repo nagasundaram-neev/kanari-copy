182196181b33c5e9ad3785e911fefe767d475e68
admin1 <admin1@neev5.(none)>
2013-07-04 14:42:13 +0530

    tablet app UI

--- app/assets/javascripts/controllers_tablet.js
+++ app/assets/javascripts/controllers_tablet.js
@@ -5,6 +5,15 @@ module.controller('commonCtrl', function($scope, $http, $location) {
 module.controller('signInController', function($scope, $http, $location) {
 	
 });
+module.controller('homePageController', function($scope, $http, $location) {
+	
+});
 module.controller('insightsController', function($scope, $http, $location) {
+	
+});
+module.controller('numericCodeController', function($scope, $http, $location) {
+	
+});
+module.controller('redemeController', function($scope, $http, $location) {
 	
 });
