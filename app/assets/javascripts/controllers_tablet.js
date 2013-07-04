3f02096ae2a4012fa1fe7158ef8e348a96431a1c
Priya <priya@neevtech.com>
2013-07-04 11:50:28 +0530

    sign up page

--- app/assets/javascripts/controllers_tablet.js
+++ app/assets/javascripts/controllers_tablet.js
@@ -1,16 +1,4 @@
-module.controller('commonCtrl', function($scope, $http, $location) {
-	//$location.url('/home');
-	$('body').removeClass('yellowBg');
-	$scope.emailCLick = function() {
-		$location.url("/login");
-	};
-	$scope.signUpCLick = function() {
-		$location.url("/signUp");
-	};
-	$scope.leaveFeedback = function() {
-		$location.url("/feedback");
-	};
-});
+
 module.controller('signUpController', function($scope, $http, $location) {
-$('body').addClass('yellowBg');
+
 });
