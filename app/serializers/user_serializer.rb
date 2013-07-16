d8dd9804471558ba7666dd5aaf5234fcd332f067
Asit Moharna <asitkmoharna@gmail.com>
2013-07-16 12:59:17 +0530

    API support for listing tablet ids

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :gender, :date_of_birth, :email, :location, :points_available, :points_redeemed
+  attributes :id, :first_name, :last_name, :gender, :date_of_birth, :email, :location, :points_available, :points_redeemed, :tablet_id
+
+  def include_tablet_id?
+    object.role == 'staff'
+  end
+  
+  def tablet_id
+    object.email.split('@')[0]
+  end
 end
