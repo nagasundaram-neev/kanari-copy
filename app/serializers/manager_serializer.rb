81d8361b035ba1d65786b286c9f590b365b51988
Asit Moharna <asitkmoharna@gmail.com>
2013-09-04 16:54:08 +0530

    Check whether a customer admin can create new outlet or not

--- app/serializers/manager_serializer.rb
+++ app/serializers/manager_serializer.rb
@@ -1,3 +1,13 @@
 class ManagerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone_number
+  attributes :id, :first_name, :last_name, :email, :phone_number, :can_create_outlet
+
+  def include_can_create_outlet?
+    ( object.role == 'customer_admin' ) ? true : false
+  end
+
+  def can_create_outlet
+    customer = object.customer
+    customer && customer.authorized_outlets > customer.outlets.count
+  end
+
 end
