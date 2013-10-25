5420ffba4378154dd38ef1ea1a9d9dc2723eef0a
Emil Soman <emil.soman@gmail.com>
2013-10-25 16:15:34 +0530

    Add mailer settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,14 @@ Kanari::Application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.default_url_options = { :host => ' kanari-377987740.ap-southeast-1.elb.amazonaws.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                 "127.0.0.1",
+    port:                    25,
+    enable_starttls_auto:    false
+  }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
