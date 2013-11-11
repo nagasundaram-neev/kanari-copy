2b3704455f90bc5162d28c47887176555dfd3819
Tushar <ntushar@neevtech.com>
2013-11-11 15:05:16 +0530

    changed the font face path in css for tablet app for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ Kanari::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
