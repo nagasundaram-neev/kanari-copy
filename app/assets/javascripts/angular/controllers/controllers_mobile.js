3af9045e6d06059558fe0ab290162437b6a3e57a
Tushar <ntushar@neevtech.com>
2013-11-19 10:44:27 +0530

    facebook app id and app secret changed to clients app for mobile app

--- app/assets/javascripts/angular/controllers/controllers_mobile.js
+++ app/assets/javascripts/angular/controllers/controllers_mobile.js
@@ -2012,7 +2012,7 @@ module.factory('Facebook', function($http, $location) {
 window.fbAsyncInit = function() {
 	FB.init({
 		//appId : '507524349327671'
-		appId : '369424903187034'
+		appId : '169570039884537'
 	});
 };
 
